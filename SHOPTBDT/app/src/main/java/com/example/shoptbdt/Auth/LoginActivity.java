9ddf795da27d5a7fe126a717b4c66ae43f724353
buibaquang0907 <buibaquang0907@gmail.com>
2023-12-31 20:17:11 +0700

    login google - fix layout

--- SHOPTBDT/app/src/main/java/com/example/shoptbdt/Auth/LoginActivity.java
+++ SHOPTBDT/app/src/main/java/com/example/shoptbdt/Auth/LoginActivity.java
@@ -13,23 +13,36 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.shoptbdt.MainActivity;
+import com.example.shoptbdt.Models.User;
 import com.example.shoptbdt.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private EditText editTextLoginEmail, editTextLoginPassword;
+    private GoogleApiClient googleApiClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        initGoogleSignIn();
         firebaseAuth = FirebaseAuth.getInstance();
         editTextLoginEmail = findViewById(R.id.editTextLoginEmail);
         editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
@@ -63,6 +76,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        com.google.android.gms.common.SignInButton loginGoogleButton = findViewById(R.id.loginGoogleButton);
+        loginGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInWithGoogle();
+            }
+        });
+
     }
 
     private void login(String email, String password) {
@@ -90,4 +111,81 @@ public class LoginActivity extends AppCompatActivity {
         editor.apply();
     }
 
+    private void initGoogleSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(LoginActivity.this, "Google Play Services Error", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    private void signInWithGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleGoogleSignInResult(result);
+        }
+    }
+
+    private void handleGoogleSignInResult(@NonNull GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+            if (account != null) {
+                String idToken = account.getIdToken();
+                firebaseAuthWithGoogle(idToken);
+            }
+        } else {
+            Toast.makeText(this, "Google Sign-In failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            User user = new User();
+                            user.setId(firebaseUser.getUid());
+                            user.setEmail(firebaseUser.getEmail());
+                            user.setName(firebaseUser.getDisplayName());
+                            user.setImage(firebaseUser.getPhotoUrl() != null ? firebaseUser.getPhotoUrl().toString() : "");
+                            saveUserLocally(user);
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void saveUserLocally(@NonNull User user) {
+        SharedPreferences preferences = getSharedPreferences("user_data", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("isLoggedIn", true);
+        editor.putString("user_id", user.getId());
+        editor.putString("user_email", user.getEmail());
+        editor.putString("user_name", user.getName());
+        editor.putString("user_image", user.getImage());
+        editor.apply();
+    }
 }
--- SHOPTBDT/app/src/main/java/com/example/shoptbdt/Fragment/HomeFragment.java
+++ SHOPTBDT/app/src/main/java/com/example/shoptbdt/Fragment/HomeFragment.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -84,7 +85,7 @@ public class HomeFragment extends Fragment implements ProductAdapter.OnProductCl
 
         // Products
         recyclerViewProducts = view.findViewById(R.id.recyclerViewProducts);
-        recyclerViewProducts.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerViewProducts.setLayoutManager(new GridLayoutManager(getContext(), 2));
         productList = new ArrayList<>();
         productAdapter = new ProductAdapter(productList, this);
         recyclerViewProducts.setAdapter(productAdapter);
