930a833115c190e176a9c0f8cc4f5402ee5ff649
nguyndinh95 <nguyencongdinh2303@gmail.com>
2024-01-09 16:31:28 +0700

    fix layout

--- SHOPTBDT/app/src/main/java/com/example/shoptbdt/Fragment/ProfileFragment.java
+++ SHOPTBDT/app/src/main/java/com/example/shoptbdt/Fragment/ProfileFragment.java
@@ -159,16 +159,16 @@ public class ProfileFragment extends Fragment {
         btnAboutUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(getActivity(), AboutUsActivity.class);
-//                startActivity(intent);
-                String name = "Quang";
-                String email = "bquangzzx@gmail.com";
-                String subject = "Test Subject";
-                String message = "Hello, this is a test email!";
-
-                // Call the sendEmail method
-                // Test Send Email
-                EmailSender.sendEmail(name, email, subject, message);
+                Intent intent = new Intent(getActivity(), AboutUsActivity.class);
+                startActivity(intent);
+//                String name = "Quang";
+//                String email = "bquangzzx@gmail.com";
+//                String subject = "Test Subject";
+//                String message = "Hello, this is a test email!";
+//
+//                // Call the sendEmail method
+//                // Test Send Email
+//                EmailSender.sendEmail(name, email, subject, message);
             }
         });
         btnChangePassword.setOnClickListener(new View.OnClickListener() {
--- SHOPTBDT/app/src/main/java/com/example/shoptbdt/Screen/FavouritesActivity.java
+++ SHOPTBDT/app/src/main/java/com/example/shoptbdt/Screen/FavouritesActivity.java
@@ -7,11 +7,9 @@ import android.os.Bundle;
 import com.example.shoptbdt.R;
 
 public class FavouritesActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
-
     }
 }
